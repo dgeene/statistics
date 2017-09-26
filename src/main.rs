c70e38f848baf6ca1661da5fd1758047ac93c48f
dgeene <david.geene@gmail.com>
2017-09-25 22:35:08 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+/*
+ * Objectives
+ *
+ * 1. sort the values
+ * 2. find the mean, median
+ * 3. find the std dev
+ * 4. find the iqr
+ * 5. read from file
+ *
+ *
+ * wishlist
+ * - read from xml
+ *
+ */
+fn main() {
+    let input = [50, 61, 44, 68, 72, 75, 64,
+        76, 84, 102, 86, 94];
+
+    //let sorted = sort();
+    sort(&input);
+    println!("Hello, world!");
+}
+
+fn sort(unsorted: &[i32]) {
+    println!("first elem: {}", unsorted[0]);
+}
